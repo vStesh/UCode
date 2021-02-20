90cbf96ca56cab8a821c39e299e165426e76576d
Vladimir Steshenko <vsteshenko@e1r2p8.unit.ua>
2021-02-20 15:46:39 +0200

    t04 done

--- Sprint04/t04/js/script.js
+++ Sprint04/t04/js/script.js
@@ -24,8 +24,11 @@ let createText = (element) => {
     //console.log(element);
     let hello = `<button id="sayHello">do magic</button><br>
     <button id="sayHello">Say Hello</button>
+    <div class="wolfBtn">
+    </div>
     <div class="description">
-    </div>`;
+    </div>
+    `;
     if(element === undefined) {
         hello = `<button id="sayHello">do magic</button><br>
         <div class="description">
@@ -37,6 +40,7 @@ let createText = (element) => {
         // sayHello.forEach(element => element.addEventListener('click', sayhello));
         return '';
     }
+    
     properties.innerHTML = '';
     properties.insertAdjacentHTML('afterbegin', hello);
     let sayHello = document.querySelectorAll('#sayHello')
@@ -63,33 +67,61 @@ let createText = (element) => {
 //head.src = magician._getPortrait();
         //createText(magician);
 
+let wolfMixin = {
+    transform () {
+        if(!document.querySelector(".howl_btn")) {
+            document.querySelector(".wolfBtn").insertAdjacentHTML('afterend','<button onclick="wolf()" class="howl_btn">howl</button>');
+            head.src = './assets/images/werewolf.png';
+        } else {
+            document.querySelector(".howl_btn").remove();
+            head.src = this._portrait;
+        }
+    },
+    thehowl() {
+        console.log('ARH-WOOOOOOOOOOOOOOOOOOOO');
+    }
+}
 
+function transformWolf() {
+    werewolf.transform();
+}
+function wolf() {
+    werewolf.thehowl();
+}
 
 function changeStatus(elem) {
     //console.log(elem);
     for (let i = 0; protoBtn[i]; i++) {
         protoBtn[i].setAttribute('class', 'protoBtn')
     }
-    let name = elem.innerHTML.split(' ')[0]
+    let name = elem.innerHTML.split(' ')[0];
     if (name === 'human') {
-        Object.setPrototypeOf(magician, human)
+        Object.setPrototypeOf(magician, human);
         head.src = magician._getPortrait();
-        createText(magician)
+        createText(magician);
         protoBtn[1].setAttribute('class', 'protoBtn active')
         
     } else if (name === 'vampire') {
-        Object.setPrototypeOf(magician, vampire)
+        Object.setPrototypeOf(magician, vampire);
         head.src = magician._getPortrait();
-        createText(magician)
+        createText(magician);
         protoBtn[2].setAttribute('class', 'protoBtn active')
     } else if (name === 'dog') {
-        Object.setPrototypeOf(magician, dog)
+        Object.setPrototypeOf(magician, dog);
         head.src = magician._getPortrait();
-        createText(magician)
+        createText(magician);
         protoBtn[3].setAttribute('class', 'protoBtn active')
+    } else if (name === 'werewolf') {
+        Object.setPrototypeOf(magician, werewolf);
+        Object.assign(Wolf.prototype, wolfMixin);
+        head.src = magician._getPortrait();
+        createText(magician);
+        document.querySelector(".wolfBtn").insertAdjacentHTML('afterbegin', `<button onclick="transformWolf()">Transform</button> <br>`);
+        protoBtn[4].setAttribute('class', 'protoBtn active');
     }
     else {
-        Object.setPrototypeOf(magician, creature)
+        Object.setPrototypeOf(magician, creature);
+        
         head.src = magician._getPortrait();
         createText();
         //protoBtn[0].setAttribute('class', 'protoBtn active')
@@ -97,40 +129,56 @@ function changeStatus(elem) {
 }
 
 
-function Creature(name, age, species) {
-    this.name = name;
-    this.age = age;
-    this.species = species;
-    this.sayHello = function () {
+class Creature {
+    constructor (name, age, species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+    
+    sayHello () {
         console.log(`Hello, my name is ${this.name}`);
     }
 }
 
 
-function Human(name, age, species, job) {
-    Creature.call(this, name, age, species);
-    this._portrait = './assets/images/human.png';
-    this.job = job;
+class Human extends Creature {
+    constructor (name, age, species, job) {
+        super(name, age, species);
+        this.job = job;
+        this._portrait = './assets/images/human.png';
+    }    
 }
 
-function Vampire(name, age, species, job, title) {
-    Creature.call(this, name, age, species);
-    this._portrait = './assets/images/vampire.png';
-    this.job = job;
-    this.title = title
+class Vampire extends Creature {
+    constructor (name, age, species, job, title) {
+        super(name, age, species);
+        this.job = job;
+        this.title = title;
+        this._portrait = './assets/images/vampire.png';
+    }
 }
 
-function Dog(name, age, species, color) {
-    Creature.call(this, name, age, species);
-    this._portrait = './assets/images/dog.png';
+class Dog extends Creature {
+    constructor (name, age, species, color) {
+        super(name, age, species);
+        this._portrait = './assets/images/dog.png';
     this.color = color;
+    }
 }
 
+class Wolf extends Human {
+    constructor (name, age, species, job) {
+        super(name, age, species);
+        this.job = job;
+        this._portrait = './assets/images/human.png';
+    }  
+}
 let creature = new Creature();
 let human = new Human('Linda', 22, 'human', 'doctor');
-let vampire = new Vampire('Vlad', 915, 'vampire', 'unemployed', 'count')
-let dog = new Dog('Fluffy', 3, 'dog', 'brown')
-
+let vampire = new Vampire('Vlad', 915, 'vampire', 'unemployed', 'count');
+let dog = new Dog('Fluffy', 3, 'dog', 'brown');
+let werewolf = new Wolf('Linda', 22, 'human', 'doctor');;
 
 let sayhello = (e) => {
     let name = e.target.innerHTML.toLowerCase();
@@ -142,8 +190,3 @@ let sayhello = (e) => {
 }
 
 createText();
-
-
-
-
-
