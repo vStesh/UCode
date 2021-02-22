d47ec5d56ccb7cf62fb9a7446c3afc440e6e783c
Vladimir Steshenko <vsteshenko@e1r11p8.unit.ua>
2021-02-22 19:00:02 +0200

    t13 start

---
+++ Sprint04/t13/to-do-list/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import TodoList from './Todo/TodoList'
+
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Купить хлеб'},
+    {id: 2, completed: false, title: 'Купить молоко'},
+    {id: 3, completed: false, title: 'Купить мяса'},
+  ])
+  
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+  return (
+    <div className="wrapper">
+      <h1>Todo list</h1>
+
+      <TodoList todos={todos} onToggle={toggleTodo}/>
+    </div>
+  );
+}
+
+export default App;
